1fabc00577a7d4d6f1b679bf83b8a23ebc362b9e
Talib <talibdolovac15@gmail.com>
2023-02-26 03:31:43 +0100

    react-router-dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import HomePage from "./Pages/HomePage/HomePage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import ContactPage from "./Pages/ContactPage/ContactPage";
 import Navigation from "./Templates/Navigation/Navigation";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </>
   );
 }
