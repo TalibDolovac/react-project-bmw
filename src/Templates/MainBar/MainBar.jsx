edb2b0a19bff667cb2e0c0bdd2314e7955e26af7
Talib <talibdolovac15@gmail.com>
2023-03-04 21:25:08 +0100

    Responsive Hamburger Menu for SM devices

--- src/Templates/MainBar/MainBar.jsx
+++ src/Templates/MainBar/MainBar.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import CustomDiv from "../../components/CustomDiv/CustomDiv";
-import { colors, fontSize } from "../../util/theme";
+import { colors, fontSize, fontWeight } from "../../util/theme";
 import { Grid } from "@mui/material";
 import bmw from "../../assets/images/Logo/bmw.png";
 import { Link } from "react-router-dom";
+import { getScreenWidth } from "../../util/helpers";
+import Text from "../../components/Text/Text";
 
 // Material ui icons
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+
+// Styles
 
 const iconStyle = {
   color: colors.secondColor,
 };
 
 const textStyle = {
-  color: colors.white,
+  LG: {
+    color: colors.white,
+  },
+  SM: {
+    color: colors.white,
+    padding: "15px",
+    textTransform: "uppercase",
+    fontWeight: fontWeight.mediumBold,
+  },
 };
 
 const LinkStyle = {
@@ -23,6 +37,8 @@ const LinkStyle = {
   display: "flex",
   gap: "3px",
 };
+
+// Styles
 
 const LeftMenu = () => (
   <CustomDiv
@@ -60,7 +76,7 @@ const RightMenu = () => (
     >
       <Link to="/" style={LinkStyle}>
         <HomeIcon style={iconStyle} />
-        <span style={textStyle}>Home</span>
+        <span style={textStyle.LG}>Home</span>
       </Link>
     </CustomDiv>
     <CustomDiv
@@ -71,7 +87,7 @@ const RightMenu = () => (
     >
       <Link to="/about" style={LinkStyle}>
         <InfoIcon style={iconStyle} />
-        <span style={textStyle}>About</span>
+        <span style={textStyle.LG}>About</span>
       </Link>
     </CustomDiv>
     <CustomDiv
@@ -82,13 +98,74 @@ const RightMenu = () => (
     >
       <Link to="/contact" style={LinkStyle}>
         <ContactSupportIcon style={iconStyle} />
-        <span style={textStyle}>Contact</span>
+        <span style={textStyle.LG}>Contact</span>
       </Link>
     </CustomDiv>
   </CustomDiv>
 );
 
-const MainBar = () => {
+const MainBarSM = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <CustomDiv
+      style={{
+        backgroundColor: colors.accentColor,
+        width: "100%",
+        height: "80px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0px 10px",
+      }}
+    >
+      <LeftMenu />
+      <CustomDiv>
+        {open ? (
+          <CloseIcon
+            onClick={handleToggle}
+            style={{ fontSize: fontSize.xLarge, color: colors.white }}
+          />
+        ) : (
+          <MenuIcon
+            onClick={handleToggle}
+            style={{ fontSize: fontSize.xLarge, color: colors.white }}
+          />
+        )}
+      </CustomDiv>
+      {open && (
+        <CustomDiv
+          style={{
+            position: "absolute",
+            top: "80px",
+            left: "0",
+            right: "0",
+            backgroundColor: colors.accentColor,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Link to="/" style={LinkStyle}>
+            <Text style={textStyle.SM}>Home</Text>
+          </Link>
+          <Link to="/about" style={LinkStyle}>
+            <Text style={textStyle.SM}>About</Text>
+          </Link>
+          <Link to="/contact" style={LinkStyle}>
+            <Text style={textStyle.SM}>Contact</Text>
+          </Link>
+        </CustomDiv>
+      )}
+    </CustomDiv>
+  );
+};
+
+const MainBarLG = () => {
   return (
     <CustomDiv
       style={{
@@ -108,6 +185,16 @@ const MainBar = () => {
         </Grid>
       </Grid>
     </CustomDiv>
+  );
+};
+
+const MainBar = () => {
+  const screenWidth = getScreenWidth();
+
+  return screenWidth === "MD" || screenWidth === "LG" ? (
+    <MainBarLG />
+  ) : (
+    <MainBarSM />
   );
 };
 
